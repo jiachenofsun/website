84045874ee1a1512ec7b84b11f80250e3f0e369c
Jiachen Sun <sunjiachen1110@gmail.com>
2023-07-28 00:56:09 +0300

    photo urls

---
+++ src/utils/photo-urls.ts
@@ -0,0 +1,9 @@
+export const photoUrls = [
+    `${process.env.PUBLIC_URL}/media/tiggy.jpg`,
+    `${process.env.PUBLIC_URL}/media/tiggy.jpg`,
+    `${process.env.PUBLIC_URL}/media/tiggy.jpg`,
+    `${process.env.PUBLIC_URL}/media/tiggy.jpg`,
+    `${process.env.PUBLIC_URL}/media/tiggy.jpg`,
+    `${process.env.PUBLIC_URL}/media/tiggy.jpg`,
+    `${process.env.PUBLIC_URL}/media/tiggy.jpg`,
+  ]
